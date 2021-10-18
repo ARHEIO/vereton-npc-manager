3735d4d4ca634d9e5299c5741a566f28879e9e9a
Adam Eggleston <adam.eggleston@estimateone.com>
2021-10-18 18:09:22 +1100

    fix linting error that broke the build

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.scss';
 import { Header } from '../components/Header';
 import { Map } from '../components/Map';
